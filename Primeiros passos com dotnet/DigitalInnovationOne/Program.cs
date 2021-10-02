8c952e6cb8ab2ba8d582e57cdc4d125e963cfeea
Joaquim Ferreira Filho <joaquimferreira@prodam.sp.gov.br>
2021-10-02 10:27:26 -0300

    atualização

--- Primeiros passos com dotnet/DigitalInnovationOne/Program.cs
+++ Primeiros passos com dotnet/DigitalInnovationOne/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace DigitalInnovationOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            int numeroDeVezes = 5;
+            for (int i = 0; i < numeroDeVezes; i++)
+            {
+                Console.WriteLine("Contador: "  + i);
+            }
+        }
+    }
+}
